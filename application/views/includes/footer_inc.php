3f1fe21c3eafb2bd164c576e34ab82e05777458f
unknown <MyDesign@.(none)>
2010-02-16 11:00:57 -0300

    prueba git maquina vale

--- application/views/includes/footer_inc.php
+++ application/views/includes/footer_inc.php
@@ -1,8 +1,4 @@
 <div class="clear span-21 push-1 last footer">
-    <div class="span-11 prepend-1">
-        <span>Vicente Lopez 2658 - (8000) Bahía Blanca</span><br/>
-        <span>Tel:(0291) 4886229/4886234 - Email:info@consurpyme.com.ar</span>
-    </div>
     <div class="span-5 append-1 last valid">
         <a href="http://jigsaw.w3.org/css-validator/validator?uri=http%3A%2F%2Fwww.consurpyme.com.ar%2F&profile=css21&usermedium=all&warning=1&lang=es"><img src="images/valid_css.png" alt="valid css"/></a>
         <a href="http://validator.w3.org/check?uri=http%3A%2F%2Fwww.consurpyme.com.ar%2F&charset=%28detect+automatically%29&doctype=Inline&group=0"><img src="images/valid_xhtml.png" alt="valid xhtml"/></a>
